ccfb44c46f7fd8c5b9beae108d36382c21b1578e
Apallo2 <85892017+Apallo2@users.noreply.github.com>
2023-02-25 19:46:37 -0800

    Tileset

--- Objects/Bullets/Bullet.gd
+++ Objects/Bullets/Bullet.gd
@@ -5,12 +5,14 @@ extends Area2D
 export var speed := 750.0
 export var damage := 1
 export var max_range := 1000.0
-export var rotation_percision := 10
 
 var _travelled_distance := 0
 
 func _physics_process(delta: float) -> void:
 	_move(delta)
+
+func rotation_percision(max_angle: float) -> void:
+	rotation += randf() * max_angle - max_angle / 2.0
 
 func _move(delta:float) -> void:
 	var distance := speed * delta
---
+++ Objects/Weapons/Pistol.gd
@@ -0,0 +1,11 @@
+extends Weapon
+
+
+
+onready var _sprite := $Sprite
+
+# Called when the node enters the scene tree for the first time.
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("shoot") and _cooldown_timer.is_stopped():
+		_cooldown_timer.start()
+		shoot()
