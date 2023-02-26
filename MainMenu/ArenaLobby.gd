ae807b71a18434fc70ed7738a65012c3336964d7
James Burns <jamesianburns@gmail.com>
2023-02-25 23:23:42 -0500

    fixed lobby bug

--- MainMenu/ArenaLobby.gd
+++ MainMenu/ArenaLobby.gd
@@ -5,12 +5,20 @@ onready var players_list = $PanelContainer/MarginContainer/LobbyContainer/Player
 var fighter_scene = preload("res://MainMenu/FighterContainer.tscn")
 
 func _ready():
-	update_players()
+	update_players(lobby.players)
 	lobby.connect("players_updated",self,"update_players")
 
-func update_players():
-	for net_id in lobby.players:
-		var p = lobby.players[net_id]
+func update_players(players):
+	print("updating players")
+	delete_children(players_list)
+	for net_id in players:
+		var p = players[net_id]
+		print("   updating player ", p.name)
 		var item = fighter_scene.instance()
 		players_list.add_child(item)
 		item.init(p.name)
+
+static func delete_children(node):
+	for n in node.get_children():
+		node.remove_child(n)
+		n.queue_free()
