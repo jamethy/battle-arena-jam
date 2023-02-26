d4f2e5dae53799a35921702b33facfad19279c8a
Apallo2 <85892017+Apallo2@users.noreply.github.com>
2023-02-25 23:41:22 -0800

    ANIMATIONS!!!

--- Objects/Player/Player.gd
+++ Objects/Player/Player.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+onready var animation_tree = $AnimationTree
+onready var animation_state = animation_tree.get("parameters/playback")
+
 export var MOTION_SPEED = 250 # Pixels/second.
 
 # Called when the node enters the scene tree for the first time.
@@ -14,7 +17,13 @@ func _physics_process(delta: float):
 	if input_dir.length_squared() > 0:
 		var velocity: Vector2 = input_dir * MOTION_SPEED
 		move_and_slide(velocity)
-
+		animation_state.travel("Run")
+	else :
+		animation_state.travel("Idle")
+	
+	animation_tree.set ('parameters/Idle/blend_position', input_dir.normalized())
+	animation_tree.set ('parameters/Run/blend_position', input_dir.normalized())
+	
 
 	
 static func get_action_iso_direction() -> Vector2:
