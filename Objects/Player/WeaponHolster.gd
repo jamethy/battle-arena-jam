e2fc76436fc792b27f7f11b8318f6127ef41e01c
James Burns <jamesianburns@gmail.com>
2023-02-26 14:52:48 -0500

    added puppet position of gun

--- Objects/Player/WeaponHolster.gd
+++ Objects/Player/WeaponHolster.gd
@@ -3,14 +3,20 @@ extends Area2D
 export var weapon_scene: PackedScene setget set_weapon
 
 var weapon: Weapon
+puppet var puppetTransform: Transform2D
 
 onready var _set_weapons_spawn_point := $WeaponSpawnPoint
 
+func _ready():
+	puppetTransform = transform
+
 func _physics_process(_delta: float):
-	if !is_network_master():
-		return
-	# This function makes the node rotate towards the mouse
-	look_at(get_global_mouse_position())
+	if is_network_master():
+		# This function makes the node rotate towards the mouse
+		look_at(get_global_mouse_position())
+		rset_unreliable("puppetTransform", transform)
+	else:
+		transform = puppetTransform
 	
 
 func set_weapon(scene: PackedScene) -> void:
