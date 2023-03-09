b3d0c4f4c1ae2294dc3548cb6f331b81d9050cdb
james <jamesianburns@gmail.com>
2023-03-09 16:40:39 -0500

    cleaned up some warnings

--- MainMenu/ColorSelector.gd
+++ MainMenu/ColorSelector.gd
@@ -1,16 +1,6 @@
 extends GridContainer
 
-
-
 var color := Color.white
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-
-	for swatch in get_children():
-		swatch.connect("pressed", self, "_on_ColorSwatch_pressed", [color])
-
 
 func _on_ColorSwatch_pressed(c: Color) -> void:
 	color = c
